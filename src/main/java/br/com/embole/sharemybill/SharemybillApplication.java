a26cb9575a67390c80dca9ed7b4ee0e43c5131a9
emanuellucascomp <cardgameheritage@gmail.com>
2023-01-25 18:24:22 -0300

    initial commit

---
+++ src/main/java/br/com/embole/sharemybill/SharemybillApplication.java
@@ -0,0 +1,13 @@
+package br.com.embole.sharemybill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharemybillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharemybillApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/embole/sharemybill/SharemybillApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.embole.sharemybill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharemybillApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
