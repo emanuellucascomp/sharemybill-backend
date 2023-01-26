92113c292fee455108029f86549a05c1f00e89f4
emanuellucascomp <cardgameheritage@gmail.com>
2023-01-25 21:23:34 -0300

    calculating taxes

--- src/main/java/br/com/embole/sharemybill/bill/application/service/BillingService.java
+++ src/main/java/br/com/embole/sharemybill/bill/application/service/BillingService.java
@@ -3,11 +3,14 @@ package br.com.embole.sharemybill.bill.application.service;
 import br.com.embole.sharemybill.bill.application.port.in.BillingUseCase;
 import br.com.embole.sharemybill.bill.application.port.in.request.BillingRequest;
 import br.com.embole.sharemybill.bill.application.port.in.request.ProductRequest;
+import br.com.embole.sharemybill.bill.application.port.in.response.BillPerCustomerResponse;
 import br.com.embole.sharemybill.bill.application.port.in.response.BillingResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -20,7 +23,33 @@ public class BillingService implements BillingUseCase {
                 .map(billPerCustomer -> calculateTotalSpentByCustomer(billPerCustomer.getConsumedProducts()))
                 .reduce(BigDecimal.ZERO, BigDecimal::add);
 
-        return null;
+        List<BillPerCustomerResponse> responsesByCustomer = new ArrayList<>();
+
+        request.getBillsPerCustomer()
+                .forEach(billPerCustomer -> {
+                    BigDecimal totalByCustomer = calculateTotalSpentByCustomer(billPerCustomer.getConsumedProducts());
+                    BigDecimal amountToPayForDelivery = totalByCustomer.multiply(request.getDeliveryAmount())
+                            .setScale(2, RoundingMode.HALF_UP)
+                            .divide(totalSpent, RoundingMode.HALF_UP)
+                            .setScale(2, RoundingMode.HALF_UP);
+                    BigDecimal amountToGetByDiscount = totalByCustomer.multiply(request.getDiscountAmount())
+                            .setScale(2, RoundingMode.HALF_UP)
+                            .divide(totalSpent, RoundingMode.HALF_UP).
+                            setScale(2, RoundingMode.HALF_UP);
+
+                    BillPerCustomerResponse responseByCustomer = BillPerCustomerResponse.builder()
+                            .amountToPay(totalByCustomer.subtract(amountToGetByDiscount).add(amountToPayForDelivery))
+                            .clientDocument(billPerCustomer.getClientDocument())
+                            .originalAmount(totalByCustomer)
+                            .totalDiscount(amountToGetByDiscount)
+                            .build();
+
+                    responsesByCustomer.add(responseByCustomer);
+                });
+
+        return BillingResponse.builder()
+                .billsPerCustomer(responsesByCustomer)
+                .build();
     }
 
     private BigDecimal calculateTotalSpentByCustomer(List<ProductRequest> consumedProducts){
